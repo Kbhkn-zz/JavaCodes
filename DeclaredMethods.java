a453de229053fef349c6dcd1438d10d3b36dceab
Kbhkn <hakan.kabasakal@bil.omu.edu.tr>
2016-11-20 14:46:41 +0300

    Fix fucking indent spaces..

--- DeclaredMethods.java
+++ DeclaredMethods.java
@@ -8,37 +8,37 @@ import java.lang.reflect.Modifier;
  */
 
 /**
-	* Example: 
-	* ThreadMXBean threadMXBean = ManagementFactory.newPlatformMXBeanProxy(remote, ManagementFactory.THREAD_MXBEAN_NAME, ThreadMXBean.class);
-	* Read all thread information of the JVM and Show the information the methods contain.
-	* 
-	*	printAllDeclaredMethodsWithValue(threadMXBean);
-	* 		getThreadCount = 20
-	* 		getPeakThreadCount = 20
-	* 		getTotalStartedThreadCount = 22
-	* 		getDaemonThreadCount = 19
+  * Example: 
+  * ThreadMXBean threadMXBean = ManagementFactory.newPlatformMXBeanProxy(remote, ManagementFactory.THREAD_MXBEAN_NAME, ThreadMXBean.class);
+  * Read all thread information of the JVM and Show the information the methods contain.
+  * 
+  * printAllDeclaredMethodsWithValue(threadMXBean);
+  *     getThreadCount = 20
+  *     getPeakThreadCount = 20
+  *     getTotalStartedThreadCount = 22
+  *     getDaemonThreadCount = 19
 */
 
 
 public class DeclaredMethods {
-	/**
-	* Show all methods with values, starting with getXXX.
-	*/
-	public static void printAllDeclaredMethodsWithValue(Object obj){
-		for (Method method : obj.getClass().getDeclaredMethods()) {
-			method.setAccessible(true);
+  /**
+  * Show all methods with values, starting with getXXX.
+  */
+  public static void printAllDeclaredMethodsWithValue(Object obj){
+    for (Method method : obj.getClass().getDeclaredMethods()) {
+      method.setAccessible(true);
 
-			if (method.getName().startsWith("get") && Modifier.isPublic(method.getModifiers())) {
-				Object value;
-				
-				try {
-					value = method.invoke(obj);
-				} catch (Exception e) {
-					value = e;
-				}
+      if (method.getName().startsWith("get") && Modifier.isPublic(method.getModifiers())) {
+        Object value;
+        
+        try {
+          value = method.invoke(obj);
+        } catch (Exception e) {
+          value = e;
+        }
 
-				System.out.println(String.format("%s = %s", method.getName(), value));
-			}
-		}
-	}
+        System.out.println(String.format("%s = %s", method.getName(), value));
+      }
+    }
+  }
 }
